5ec46faada830fc3fa98a4b7ad64c425067bc68b
Frederik Gladhorn <frederik.gladhorn@digia.com>
2013-02-15 16:58:19 +0100

    Add files

---
+++ src/gitcommitlist.cpp
@@ -0,0 +1,2 @@
+
+#include "gitcommitlist.h"
---
+++ src/gitcommitlist.h
@@ -0,0 +1,15 @@
+
+
+#ifndef GITCOMMITLIST_H
+#define GITCOMMITLIST_H
+
+#include <git2.h>
+
+#include <QStandardItemModel>
+
+class GitCommitList : public QStandardItemModel
+{
+
+};
+
+#endif
